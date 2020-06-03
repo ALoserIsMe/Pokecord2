aa7eb8ec23a10b5623049047302066490dac7dde
ALoserIsMe <32465750+ALoserIsMe@users.noreply.github.com>
2020-06-03 16:15:39 +0100

    Update pokedex.py

--- pokedex.py
+++ pokedex.py
@@ -23,6 +23,7 @@ class pokemon:
         self.ability       = ""
         self.hiddenAbility = ""
         self.hidAbBool     = False
+        self.maxLevel      = 0
 
     def generateNature(self):
         natures = ["Hardy", "Lonely", "Brave", "Adamant", "Naughty", "Bold", "Docile", "Relaxed", "Impish", "Lax", "Timid", "Hasty", "Serious", "Jolly", "Naive", "Modest", "Mild", "Quiet", "Bashful", "Rash", "Calm", "Gentle", "Sassy", "Careful", "Quirky"]
