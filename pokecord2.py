7d18b30af4047a55c5383068080a8992cec884bf
ALoserIsMe <32465750+ALoserIsMe@users.noreply.github.com>
2020-06-03 14:40:33 +0100

    Update pokecord2.py

--- pokecord2.py
+++ pokecord2.py
@@ -4,51 +4,60 @@ import asyncio
 import random
 import time
 
+players = []
+
+# Stores the current pokemon, as well as spawning a new one
 class pokecord2:
     def __init__(self):
         self.currentPokemon = ""
+        self.caught = False
+
         self.allPokemon = [bulbasaur(), charmander(), squirtle()]
 
     def spawnPokemon(self):
         self.currentPokemon = random.choice(self.allPokemon)
         print("Current Pokemon:", self.currentPokemon.getSpecies())
-
-'''allPokemon = [bulbasaur(), charmander(), squirtle()]
-currentPokemon = ""'''
+        self.caught = False
 
 pokecord = pokecord2()
 bot = commands.Bot(command_prefix = "p!")
 
+# Spawns a pokemon, then waits between 5 and 30 mins before repeating
 async def background_loop():
     mins5  = 300
     mins30 = 1800
     await bot.wait_until_ready()
     print("background loop ready")
     while True:
-        '''currentPokemon = random.choice(allPokemon)
-        print("Pokemon:", currentPokemon.getPokedex())'''
         pokecord.spawnPokemon()
         channel = bot.get_channel(716961981579526308)
-        await channel.send(pokecord.currentPokemon.getPokedex())
+#        await channel.send("```**A wild pokemon has appeared!** \nGuess the pokemon and type p!catch <pokemon> to catch it!\n" + str(pokecord.currentPokemon.getPokedex()) + "```")
+        await channel.send("**A wild pokemon has appeared!** \nGuess the pokemon and type p!catch <pokemon> to catch it!\n" + str(pokecord.currentPokemon.getPokedex()))
 
         time = random.randint(mins5, mins30)
         print("Waiting", time, "seconds")
         await asyncio.sleep(time)
 
+# If the user's message is the right name for the pokemon, congratulate them. Else, tell them they are wrong.
+# If the pokemon has already been caught, tell the user.
 @bot.command()
 async def catch(ctx, arg):
-    if arg.lower() == pokecord.currentPokemon.getSpecies().lower():
-        await ctx.send("caught: " + pokecord.currentPokemon.getSpecies())
+    if pokecord.caught == False:
+        if arg.lower() == pokecord.currentPokemon.getSpecies().lower():
+            await ctx.send("caught: " + pokecord.currentPokemon.getSpecies())
+            pokecord.caught = True
+        else:
+            await ctx.send("That is the wrong pokemon.")
     else:
-        await ctx.send("That is the wrong pokemon.")
-
-@bot.command()
-async def echo(ctx, arg):
-    await ctx.send(arg)
+        await ctx.send("The pokemon has already been caught")
 
 @bot.event
 async def on_ready():
     print("Ready")
 
+@bot.event
+async def on_guild_join():
+    print(Guild.name)
+
 bot.loop.create_task(background_loop())
-bot.run("TOKEN", bot=True)
+bot.run("NzE3MDY4Mjc3NjY2MjgzNTUx.XtVX_Q.ljn3RE4DCR-yUp6kpaoI6XJU3qQ", bot=True)
